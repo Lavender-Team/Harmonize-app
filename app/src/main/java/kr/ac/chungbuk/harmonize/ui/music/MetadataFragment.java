6b5ef8afa8473260e413c7329129af61c8cef1b3
Woochang Lee <changi112242@gmail.com>
2024-03-08 22:40:15 +0900

    feat: 비슷한 곡들 레이아웃 추가

--- app/src/main/java/kr/ac/chungbuk/harmonize/ui/music/MetadataFragment.java
+++ app/src/main/java/kr/ac/chungbuk/harmonize/ui/music/MetadataFragment.java
@@ -2,13 +2,18 @@ package kr.ac.chungbuk.harmonize.ui.music;
 
 import android.os.Bundle;
 
+import androidx.databinding.ObservableArrayList;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import kr.ac.chungbuk.harmonize.R;
+import kr.ac.chungbuk.harmonize.entity.SimpleMusic;
+import kr.ac.chungbuk.harmonize.utility.adapter.LatestMusicListAdapter;
 
 public class MetadataFragment extends Fragment {
 
@@ -52,10 +57,37 @@ public class MetadataFragment extends Fragment {
         }
     }
 
+
+    RecyclerView relatedMusicListView;
+    LinearLayoutManager relatedMusicLayoutManager;
+    LatestMusicListAdapter relatedMusicAdapter;
+
+    private ObservableArrayList<SimpleMusic> relatedMusics = new ObservableArrayList<>();
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_metadata, container, false);
+        View root = inflater.inflate(R.layout.fragment_metadata, container, false);
+
+
+        relatedMusicListView = root.findViewById(R.id.relatedMusicListView);
+
+        /* 이 노래와 비슷한 곡들 */
+        relatedMusicLayoutManager = new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL, false);
+        relatedMusicAdapter = new LatestMusicListAdapter(relatedMusics);
+        relatedMusicListView.setLayoutManager(relatedMusicLayoutManager);
+        relatedMusicListView.setAdapter(relatedMusicAdapter);
+
+
+        relatedMusics.clear();
+        relatedMusics.add(new SimpleMusic("사랑할 수 밖에", "볼빨간 사춘기"));
+        relatedMusics.add(new SimpleMusic("화이트(White)", "폴킴"));
+        relatedMusics.add(new SimpleMusic("주저하는 연인들을 위해", "잔나비"));
+        relatedMusics.add(new SimpleMusic("사랑할 수 밖에", "볼빨간 사춘기"));
+        relatedMusics.add(new SimpleMusic("화이트(White)", "폴킴"));
+        relatedMusicAdapter.notifyDataSetChanged();
+
+        return root;
     }
 }
